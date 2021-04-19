b3b5d616fa697589963aa20aa5a3733311648fb8
tcinbis <tcinbis@gmail.com>
2021-04-19 10:07:10 +0200

    Making golint-ci happy

--- internal/congestion/interface.go
+++ internal/congestion/interface.go
@@ -36,7 +36,7 @@ type CubicInterface interface {
 }
 
 type FlowTeleCongestionControlModifier interface {
-	ApplyControl(beta float64, cwnd_adjust int64, cwnd_max_adjust int64, use_conservative_allocation bool) bool
+	ApplyControl(beta float64, cwnd_adjust int64, cwnd_max_adjust int64, use_conservative_allocation bool) bool //nolint:stylecheck
 
 	SetFixedRate(rateInBytePerSecond Bandwidth)
 }
