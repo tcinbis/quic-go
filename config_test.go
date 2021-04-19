1c4a445e4ad873d59dfb40e00c7bb075132d08d4
tcinbis <tcinbis@gmail.com>
2021-04-19 09:36:05 +0200

    Fixing failing config_test.go
    The test has to handle the setup of the configuration field `FlowTeleSignal`.

--- config_test.go
+++ config_test.go
@@ -2,6 +2,7 @@ package quic
 
 import (
 	"fmt"
+	"github.com/lucas-clemente/quic-go/flowtele"
 	"net"
 	"reflect"
 	"time"
@@ -81,6 +82,8 @@ var _ = Describe("Config", func() {
 				f.Set(reflect.ValueOf(true))
 			case "Tracer":
 				f.Set(reflect.ValueOf(mocklogging.NewMockTracer(mockCtrl)))
+			case "FlowTeleSignal":
+				f.Set(reflect.ValueOf(&flowtele.FlowTeleSignal{}))
 			default:
 				Fail(fmt.Sprintf("all fields must be accounted for, but saw unknown field %q", fn))
 			}
