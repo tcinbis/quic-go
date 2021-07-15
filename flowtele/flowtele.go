9a1bae4bd2f05d00e06f2a99f06da8a75b2f1f35
tcinbis <tcinbis@gmail.com>
2021-07-15 17:21:16 +0200

    Adding new packets lost ratio dbus signal

--- flowtele/flowtele.go
+++ flowtele/flowtele.go
@@ -4,6 +4,6 @@ import (
 	"time"
 )
 
-func CreateFlowteleSignalInterface(newSrttMeasurement func(t time.Time, srtt time.Duration), packetsLost func(t time.Time, newSlowStartThreshold uint64), packetsAcked func(t time.Time, congestionWindow uint64, packetsInFlight uint64, ackedBytes uint64)) *FlowTeleSignal {
-	return &FlowTeleSignal{NewSrttMeasurement: newSrttMeasurement, PacketsLost: packetsLost, PacketsAcked: packetsAcked}
+func CreateFlowteleSignalInterface(newSrttMeasurement func(t time.Time, srtt time.Duration), packetsLost func(t time.Time, newSlowStartThreshold uint64), packetsLostRatio func(t time.Time, lostRatio float64), packetsAcked func(t time.Time, congestionWindow uint64, packetsInFlight uint64, ackedBytes uint64)) *FlowTeleSignal {
+	return &FlowTeleSignal{NewSrttMeasurement: newSrttMeasurement, PacketsLost: packetsLost, PacketLostRatio: packetsLostRatio, PacketsAcked: packetsAcked}
 }
--- flowtele/flowtele_signal.go
+++ flowtele/flowtele_signal.go
@@ -2,9 +2,9 @@ package flowtele
 
 import "time"
 
-// TODO: Check if this is the proper location for the struct
 type FlowTeleSignal struct {
 	NewSrttMeasurement func(t time.Time, srtt time.Duration)
 	PacketsLost        func(t time.Time, newSlowStartThreshold uint64)
+	PacketLostRatio    func(t time.Time, lostRatio float64)
 	PacketsAcked       func(t time.Time, congestionWindow uint64, packetsInFlight uint64, ackedBytes uint64)
 }
