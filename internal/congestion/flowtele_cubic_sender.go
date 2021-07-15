eb417aae25fa9d4097be76c2622b1a77731da4c6
tcinbis <tcinbis@gmail.com>
2021-07-15 13:03:34 +0200

    Ignoring ApplyControl if in slowstart

--- internal/congestion/flowtele_cubic_sender.go
+++ internal/congestion/flowtele_cubic_sender.go
@@ -77,7 +77,11 @@ func (c *flowTeleCubicSender) slowStartThresholdUpdated() {
 }
 
 func (c *flowTeleCubicSender) ApplyControl(beta float64, cwnd_adjust int64, cwnd_max_adjust int64, use_conservative_allocation bool) bool { //nolint:stylecheck
-	fmt.Printf("FLOWTELE CC: ApplyControl(%f, %d, %d, %t) mod\n", beta, cwnd_adjust, cwnd_max_adjust, use_conservative_allocation)
+	if c.InSlowStart() {
+		fmt.Printf("FLOWTELE CC: IGNORING because in slow start! ApplyControl(%f, %d, %d, %t)\n", beta, cwnd_adjust, cwnd_max_adjust, use_conservative_allocation)
+		return true
+	}
+	fmt.Printf("FLOWTELE CC: ApplyControl(%f, %d, %d, %t)\n", beta, cwnd_adjust, cwnd_max_adjust, use_conservative_allocation)
 	flowTeleCubic := c.checkFlowTeleCubicAlgorithm()
 
 	flowTeleCubic.lastMaxCongestionWindowAddDelta = cwnd_max_adjust
