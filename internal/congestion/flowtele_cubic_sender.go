ae220d171dd6cfeb063ecbd8d6ce4c2ccebbe55e
tcinbis <tcinbis@gmail.com>
2021-07-15 17:28:56 +0200

    Removing print statement

--- internal/congestion/flowtele_cubic_sender.go
+++ internal/congestion/flowtele_cubic_sender.go
@@ -180,7 +180,6 @@ func (c *flowTeleCubicSender) OnPacketLost(packetNumber protocol.PacketNumber, l
 	c.numPacketsLost++
 	lostRatio := float64(c.numPacketsLost) / (float64(c.largestAckedPacketNumber) - float64(c.numPacketsLost))
 	go c.flowTeleSignalInterface.PacketLostRatio(time.Now(), lostRatio)
-	fmt.Printf("Total packets: %d Packets lost: %d Ratio: %f %%\n", int(c.largestAckedPacketNumber), c.numPacketsLost, lostRatio*100)
 	// reset packet count from congestion avoidance mode. We start
 	// counting again when we're out of recovery.
 	c.numAckedPackets = 0
