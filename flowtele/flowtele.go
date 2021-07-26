aa7d3501e7e2780ff440c1816d2b89b60f18f9a8
tcinbis <tcinbis@gmail.com>
2021-07-26 19:53:15 +0200

    Handling nil function pointers in flowtele

--- flowtele/flowtele.go
+++ flowtele/flowtele.go
@@ -5,5 +5,21 @@ import (
 )
 
 func CreateFlowteleSignalInterface(newSrttMeasurement func(t time.Time, srtt time.Duration), packetsLost func(t time.Time, newSlowStartThreshold uint64), packetsLostRatio func(t time.Time, lostRatio float64), packetsAcked func(t time.Time, congestionWindow uint64, packetsInFlight uint64, ackedBytes uint64)) *FlowTeleSignal {
+	if newSrttMeasurement == nil {
+		newSrttMeasurement = func(t time.Time, srtt time.Duration) {}
+	}
+
+	if packetsLost == nil {
+		packetsLost = func(t time.Time, newSlowStartThreshold uint64) {}
+	}
+
+	if packetsLostRatio == nil {
+		packetsLostRatio = func(t time.Time, lostRatio float64) {}
+	}
+
+	if packetsAcked == nil {
+		packetsAcked = func(t time.Time, congestionWindow uint64, packetsInFlight uint64, ackedBytes uint64) {}
+	}
+
 	return &FlowTeleSignal{NewSrttMeasurement: newSrttMeasurement, PacketsLost: packetsLost, PacketLostRatio: packetsLostRatio, PacketsAcked: packetsAcked}
 }
